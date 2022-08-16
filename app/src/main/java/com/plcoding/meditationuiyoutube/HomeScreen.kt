2ec99c8b65d336d37dbdbbe04e2b9a2aa508aade
andrenivisnk <andrenivinsk@gmail.com>
2022-08-16 17:43:12 +0300

    Greeting section done

--- app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
+++ app/src/main/java/com/plcoding/meditationuiyoutube/HomeScreen.kt
@@ -1,2 +1,57 @@
 package com.plcoding.meditationuiyoutube
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.plcoding.meditationuiyoutube.ui.theme.DeepBlue
+
+@Composable
+fun HomeScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(DeepBlue)) {
+        Column {
+            GreetingSection()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingSection(name: String = "Andrey") {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
